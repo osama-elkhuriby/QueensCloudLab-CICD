4178d2efe4212d317db86485f4afe775642c6cf4
Osama Elkhuribi <41060966+osama-elkhuriby@users.noreply.github.com>
2026-04-07 12:40:27 +0200

    Reformat insertMany for task items in init.js

--- db/init.js
+++ db/init.js
@@ -6,5 +6,5 @@ db.tasks.insertMany([
   { id: 4, name: 'Butter',       status: 'pending' },
   { id: 5, name: 'Orange juice', status: 'pending' },
   { id: 6, name: 'Coffee',       status: 'pending' },
-  { id: 7, name: 'Tea',          status: 'pending' }
+  { id: 7, name: 'Tea',          status: 'pending' } // New
 ]);
